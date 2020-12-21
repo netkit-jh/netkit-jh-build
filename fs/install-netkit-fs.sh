6a49002ecdf48f6776f3cb519b76a3b172ea7976
TechSupportJosh <joshuahawking1@gmail.com>
2020-12-21 21:00:42 +0000

    Removed rootstrap from the building process.

---
+++ fs/install-netkit-fs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# First argument is the work directory, that contains the packages list, package selections, service lists
+WORK_DIRECTORY="$1"
+# Second argument is the build directory, that contains the filesystem version file
+BUILD_DIRECTORY="$2"
+# Third argument is the directory which the filesystem is mounted at
+MOUNT_DIRECTORY="$3"
+# Forth argument is the kernel module directory which should be copied
+KERNEL_MODULES="$4"
+
+whoami
+
+# Load debconf-package-selections
+cat $WORK_DIRECTORY/debconf-package-selections | chroot $MOUNT_DIRECTORY debconf-set-selections
+
+# Install packages in packages-list
+PACKAGES_LIST=`cat $WORK_DIRECTORY/packages-list | grep -v '#'`
+
+chroot $MOUNT_DIRECTORY apt update
+chroot $MOUNT_DIRECTORY apt install --assume-yes ${PACKAGES_LIST}
+
+# Now install any additional packages which require specific options
+# We want wireguard without installing wireguard-dkms because we have it built into the kernel tree
+chroot $MOUNT_DIRECTORY apt install --no-install-recommends wireguard-tools
+
+# We want iptables legacy, rather than nftables
+chroot $MOUNT_DIRECTORY update-alternatives --set iptables /usr/sbin/iptables-legacy
+
+# Copy netkit filesystem files
+tar -C $WORK_DIRECTORY/filesystem-tweaks -c . | tar --overwrite --same-owner -C $MOUNT_DIRECTORY -x
+
+# Copy in version file
+cp $BUILD_DIRECTORY/netkit-filesystem-version $MOUNT_DIRECTORY/etc/netkit-filesystem-version
+
+# Create kernel module directory
+mkdir -p $MOUNT_DIRECTORY/lib/modules
+
+# Copy in kernel modules (unused as kernel modules are mounted at runtime)
+# cp -r $KERNEL_MODULES/* /lib/modules/
+
+# Install netkit services
+chroot $MOUNT_DIRECTORY systemctl enable netkit-startup-phase1.service
+chroot $MOUNT_DIRECTORY systemctl enable netkit-startup-phase2.service
+chroot $MOUNT_DIRECTORY systemctl enable netkit-shutdown.service
+chroot $MOUNT_DIRECTORY systemctl enable netkit-mount.service
+chroot $MOUNT_DIRECTORY systemctl enable netkit-unmount.service
+
+# Disable system services not required
+for SERVICE in `cat $WORK_DIRECTORY/disabled-services`; do
+	chroot $MOUNT_DIRECTORY systemctl disable ${SERVICE}
+done
+
+# Save debconf-package-selections
+chroot $MOUNT_DIRECTORY debconf-get-selections > $WORK_DIRECTORY/build/debconf-package-selections.last
+
+# Empty caches
+chroot $MOUNT_DIRECTORY apt clean
