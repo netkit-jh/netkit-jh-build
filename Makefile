96714ef916f14714459b7751f0bddfbec4dcb2f2
TechSupportJosh <joshuahawking1@gmail.com>
2020-12-22 14:02:32 +0000

    Added install script to build process.

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CORE_DIR=core
 
 include Makefile.am
 
-default: build-kernel build-fs build-core release.sha256
+default: build-kernel build-fs build-core release.sha256 install-netkit-jh-${NETKIT_BUILD_RELEASE}.sh
 
 .PHONY: build-fs
 build-fs: ${FS_ARCHIVE_FILE}
@@ -26,6 +26,10 @@ ${CORE_ARCHIVE_FILE}:
 
 release.sha256:
 	sha256sum ${FS_ARCHIVE_FILE} ${KERNEL_ARCHIVE_FILE} ${CORE_ARCHIVE_FILE} > release.sha256
+
+install-netkit-jh-${NETKIT_BUILD_RELEASE}.sh:
+	cp install-netkit-jh.sh install-netkit-jh-${NETKIT_BUILD_RELEASE}.sh
+	sed -i "s/VERSION=\"VERSION_NUMBER\"/VERSION=\"${NETKIT_BUILD_RELEASE}\"/g" install-netkit-jh-${NETKIT_BUILD_RELEASE}.sh
 	
 .PHONY: clean
 clean:
@@ -33,6 +37,7 @@ clean:
 	$(MAKE) -C ${FS_DIR} clean
 	$(MAKE) -C ${CORE_DIR} clean
 	rm -f release.sha256
+	rm -f install-netkit-jh-${NETKIT_BUILD_RELEASE}.sh
 
 .PHONY: mrproper
 mrproper: clean
@@ -40,3 +45,4 @@ mrproper: clean
 	$(MAKE) -C ${FS_DIR} mrproper
 	$(MAKE) -C ${CORE_DIR} mrproper
 	rm -f release.sha256
+	rm -f install-netkit-jh-${NETKIT_BUILD_RELEASE}.sh
---
+++ install-netkit-jh.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Download and install netkit-ng in home directory
+# Peter Norris: 22 Nov 2020
+# Adapted for Josh Hawking's development to provide an up-to-date kernel
+# https://github.com/TechSupportJosh/netkit-ng-build/releases/tag/0.1.7
+
+UNZIP_TARGET_DIR="${HOME}/netkit-jh"
+DOWNLOAD_DIR="/tmp"
+VERSION="VERSION_NUMBER"
+
+# rename target dir if already exists
+if [ -d "$UNZIP_TARGET_DIR" ] ; then
+	BACKUP_TARGET_DIR="${UNZIP_TARGET_DIR}-$(date '+%F_%H-%M-%S')"
+	echo "$UNZIP_TARGET_DIR already exists. Renaming it to $BACKUP_TARGET_DIR so contents are not disturbed."
+	mv "$UNZIP_TARGET_DIR" "$BACKUP_TARGET_DIR"
+fi
+
+mkdir -p "$UNZIP_TARGET_DIR"
+mkdir -p "$DOWNLOAD_DIR"
+
+# CD to download directory to download files
+cd "${DOWNLOAD_DIR}"
+
+# download netkit files
+# core, kernel and file system from Josh Hawking's updated work
+wget -O release.sha256 --show-progress "https://github.com/TechSupportJosh/netkit-ng-build/releases/download/$VERSION/release.sha256"
+
+wget -O netkit-core.tar.bz2 --show-progress "https://github.com/TechSupportJosh/netkit-ng-build/releases/download/$VERSION/netkit-core-$VERSION.tar.bz2"
+wget -O netkit-fs.tar.bz2 --show-progress "https://github.com/TechSupportJosh/netkit-ng-build/releases/download/$VERSION/netkit-fs-$VERSION.tar.bz2"
+wget -O netkit-kernel.tar.bz2 --show-progress "https://github.com/TechSupportJosh/netkit-ng-build/releases/download/$VERSION/netkit-kernel-$VERSION.tar.bz2"
+
+if ! sha256sum -c release.sha256; then
+    echo "File checksums: FAILED" >&2
+    exit 1
+fi
+
+echo "File checksums: OK"
+
+echo "Extracting files..."
+# strip-components removes the netkit-ng directory
+# .tar.bz2
+tar -xjvC "$UNZIP_TARGET_DIR" --strip-components=1 -f netkit-core.tar.bz2
+tar -xjvC "$UNZIP_TARGET_DIR" --strip-components=1 -f netkit-fs.tar.bz2
+tar -xjvC "$UNZIP_TARGET_DIR" --strip-components=1 -f netkit-kernel.tar.bz2
+ 
+# back up existing bashrc file with date and time as part of filename
+BASHBAK="${HOME}/bashrc_$(date "+%F_%H-%M-%S").bak"
+cp "${HOME}/.bashrc" "$BASHBAK"
+
+# strip out any lines containing the word "netkit" (case insensitive) from bashrc
+grep -iv "netkit" "$BASHBAK" > "${HOME}/.bashrc"
+
+# use heredoc (with tab suppression using the <<- form) to append netkit additions to bashrc  
+cat >> "${HOME}/.bashrc" <<-EOF
+	# additions for netkit
+	export NETKIT_HOME="$UNZIP_TARGET_DIR"
+	export MANPATH="\$MANPATH:\$NETKIT_HOME/man"
+	export PATH="\$PATH:\$NETKIT_HOME/bin"
+EOF
+
+# make (for lab.dep) and net-tools (for tap) needed on ubuntu 18.04
+echo "Installing packages to run netkit..."
+sudo apt-get update && sudo apt-get install xterm make net-tools wireshark
+
+
+# check netkit install now works
+# TODO: Get this to work... Netkit installs fine, but the check-configurator script doesn't read the environment variables properly so thinks something is wrong
+source ~/.bashrc
+cd "$UNZIP_TARGET_DIR"
+./check_configuration.sh
+
+# encourage user to set environment variables for the current bash terminal
+echo "Future terminals that you launch will automatically get the netkit settings."
+echo "To make the netkit settings available in this terminal, run the following command:"
+echo "source ~/.bashrc"
+
+echo -e "\033[1mRun ${UNZIP_TARGET_DIR}/change-terminal.sh to change your terminal emulator (highly recommended\!)\033[0m"
