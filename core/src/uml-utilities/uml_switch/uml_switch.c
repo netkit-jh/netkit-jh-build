08d8ae24c3c8be6eddd1e51264564bd36886c2d4
Adam Bromiley <adam@bromiley.co.uk>
2021-02-02 15:10:10 +0000

    add uml_switch header

--- core/src/uml-utilities/uml_switch/uml_switch.c
+++ core/src/uml-utilities/uml_switch/uml_switch.c
@@ -24,59 +24,10 @@
 #include <stddef.h>
 #endif
 
+#include "uml_switch.h"
+
 static int hub = 0;
 static int compat_v0 = 0;
-
-enum request_type { REQ_NEW_CONTROL };
-
-struct request_v0 {
-  enum request_type type;
-  union {
-    struct {
-      unsigned char addr[ETH_ALEN];
-      struct sockaddr_un name;
-    } new_control;
-  } u;
-};
-
-#define SWITCH_MAGIC 0xfeedface
-
-struct request_v1 {
-  uint32_t magic;
-  enum request_type type;
-  union {
-    struct {
-      unsigned char addr[ETH_ALEN];
-      struct sockaddr_un name;
-    } new_control;
-  } u;
-};
-
-struct request_v2 {
-  uint32_t magic;
-  uint32_t version;
-  enum request_type type;
-  struct sockaddr_un sock;
-};
-
-struct reply_v2 {
-  unsigned char mac[ETH_ALEN];
-  struct sockaddr_un sock;
-};
-
-struct request_v3 {
-  uint32_t magic;
-  uint32_t version;
-  enum request_type type;
-  struct sockaddr_un sock;
-};
-
-union request {
-  struct request_v0 v0;
-  struct request_v1 v1;
-  struct request_v2 v2;
-  struct request_v3 v3;
-};
 
 static char *ctl_socket = "/tmp/uml.ctl";
 
