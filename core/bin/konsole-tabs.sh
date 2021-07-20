e31299f54a59418e2026477ed9915de66e6d17d0
Oshawk <oshawk@protonmail.com>
2021-07-20 19:05:37 -0400

    Update regex (#79)
    * Update regex
    
    * Make parameter assignment consistent
    
    * Fix misplaced space
    
    * Fix redirection
    
    * Fix awk expression
    
    * Make requested changes
    
    Made the requested changes to the `sed` command and utility functions.
    
    Used the `printf` method as it doesn't append a newline whereas `<<<` does. Also replaced `$1` with `$*` to support multiple arguments as suggested.

--- core/bin/konsole-tabs.sh
+++ core/bin/konsole-tabs.sh
@@ -164,7 +164,7 @@ if [ ! -z "$TERM_TITLE" ]; then
    VM_CONSOLE_NUMBER=${VM_CONSOLE_NUMBER% (*)}
    VM_TITLE="${VM_NAME} (con${VM_CONSOLE_NUMBER})"
 else
-   VM_TITLE=$(echo $EXECUTE_COMMAND | sed -r 's/^.*umid=([^ ]+).*$/\1/')
+   VM_TITLE=$(echo "$EXECUTE_COMMAND" | sed -r 's/^.*umid=([^ ]+).*$/\1/')
 fi
 
 qdbus $NETKIT_KONSOLE /Sessions/$SESSION setTitle 1 "${VM_TITLE}" >/dev/null
