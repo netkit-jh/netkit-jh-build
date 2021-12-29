6a89cc1fc917e1301f628e4528fd6a5cc34b4bba
Adam Bromiley <adam@bromiley.co.uk>
2021-12-29 13:22:40 +0000

    append forward slash to path

--- core/setup_scripts/check_configuration.d/03-check_man_pages.sh
+++ core/setup_scripts/check_configuration.d/03-check_man_pages.sh
@@ -33,7 +33,7 @@ echo -n ">  Checking for availability of man pages... "
 # trailing and back-to-back forward slashes, and more importantly symbolic
 # links. This means the test depends on the man/ directory having netkit.7.
 if [ "$(man --where 7 netkit)" -ef "$NETKIT_HOME/man/man7/netkit.7" ]; then
-   new_manpath="${MANPATH:+"\$MANPATH:"}:$NETKIT_HOME/man"
+   new_manpath="${MANPATH:+"\$MANPATH:"}:$NETKIT_HOME/man/"
    
    cat << END_OF_DIALOG
 failed.
