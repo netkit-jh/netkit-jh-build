7b4fa0daad12ba1623af1e9b6edbef886fdefdc1
Adam Bromiley <adam@bromiley.co.uk>
2022-01-01 21:09:50 +0000

    rename mount symbols to mnt

--- fs/install-netkit-fs.sh
+++ fs/install-netkit-fs.sh
@@ -48,7 +48,7 @@ work_dir=$1
 build_dir=$2
 
 # Mount point of the filesystem
-mount_dir=$3
+mnt_point=$3
 
 # Kernel module directory to copy into the filesystem at /lib/modules
 kernel_modules=$4
@@ -56,27 +56,27 @@ kernel_modules=$4
 
 ### INSTALL APT PACKAGES ######################################################
 # Load debconf-package-selections
-chroot -- "$mount_dir" debconf-set-selections < "$work_dir/debconf-package-selections"
+chroot -- "$mnt_point" debconf-set-selections < "$work_dir/debconf-package-selections"
 
 # Install add-apt-repository command
-chroot -- "$mount_dir" apt-get update
-chroot -- "$mount_dir" apt-get --assume-yes install software-properties-common
+chroot -- "$mnt_point" apt-get update
+chroot -- "$mnt_point" apt-get --assume-yes install software-properties-common
 
 # Add additional repositories in this section
-# chroot -- "$mount_dir" add-apt-repository ...
-# chroot -- "$mount_dir" apt-get update
+# chroot -- "$mnt_point" add-apt-repository ...
+# chroot -- "$mnt_point" apt-get update
 
 # Install packages listed in packages-list
 mapfile -t packages_list < <(grep --invert-match -- "^#" "$work_dir/packages-list")
-chroot -- "$mount_dir" apt-get --assume-yes -- install "${packages_list[@]}"
+chroot -- "$mnt_point" apt-get --assume-yes -- install "${packages_list[@]}"
 
 # Install packages which require specific APT options
 # We want wireguard without installing wireguard-dkms because we have it built
 # into the kernel tree.
-chroot -- "$mount_dir" apt-get --no-install-recommends install wireguard-tools
+chroot -- "$mnt_point" apt-get --no-install-recommends install wireguard-tools
 
 # Install legacy iptables over nftables
-chroot -- "$mount_dir" update-alternatives --set iptables /usr/sbin/iptables-legacy
+chroot -- "$mnt_point" update-alternatives --set iptables /usr/sbin/iptables-legacy
 
 
 ### COPY OVER PRECONFIGURED FILES FOR NETKIT ##################################
@@ -88,25 +88,25 @@ cp \
    --no-preserve=mode,ownership \
    --no-dereference \
    --preserve=links \
-   --target-directory "$mount_dir" \
+   --target-directory "$mnt_point" \
    -- \
    "$work_dir/filesystem-tweaks/"*
 
 # Some files in filesystem-tweaks need to be executable, so we set their mode
 # correctly here.
 fs_tweaks_executables=(
-   "$mount_dir/usr/local/bin/tcpdump"
-   "$mount_dir/etc/netkit/"*
+   "$mnt_point/usr/local/bin/tcpdump"
+   "$mnt_point/etc/netkit/"*
 )
 chmod -- +x "${fs_tweaks_executables[@]}"
 
 # Copy the version file in
-cp -- "$build_dir/netkit-filesystem-version" "$mount_dir/etc/netkit-filesystem-version"
+cp -- "$build_dir/netkit-filesystem-version" "$mnt_point/etc/netkit-filesystem-version"
 
 
 ### MANAGE SERVICES ###########################################################
 # Enable Netkit services
-chroot -- "$mount_dir" systemctl enable \
+chroot -- "$mnt_point" systemctl enable \
    netkit-startup-phase1.service \
    netkit-startup-phase2.service \
    netkit-shutdown.service
@@ -115,43 +115,43 @@ chroot -- "$mount_dir" systemctl enable \
 ln \
    --symbolic \
    -- \
-   "$mount_dir/lib/systemd/system/getty@.service" \
-   "$mount_dir/etc/systemd/system/getty.target.wants/getty@tty0.service"
+   "$mnt_point/lib/systemd/system/getty@.service" \
+   "$mnt_point/etc/systemd/system/getty.target.wants/getty@tty0.service"
 
-chroot -- "$mount_dir" systemctl mask getty-static "getty@tty"{2..6}".service"
+chroot -- "$mnt_point" systemctl mask getty-static "getty@tty"{2..6}".service"
 
 # Disable autostart for some services
 mapfile -t disabled_services < <(grep --invert-match -- "^#" "$work_dir/disabled-services")
-chroot -- "$mount_dir" systemctl disable "${disabled_services[@]}"
+chroot -- "$mnt_point" systemctl disable "${disabled_services[@]}"
 
 
 ### INSTALL KERNEL MODULES ####################################################
 # Create kernel module directory
-mkdir --parents -- "$mount_dir/lib/modules"
+mkdir --parents -- "$mnt_point/lib/modules"
 
 # Copy in kernel modules (kernel modules can instead by mounted at runtime by
 # enabling the netkit-mount service).
-cp --recursive -- "$kernel_modules/"* "$mount_dir/lib/modules/"
+cp --recursive -- "$kernel_modules/"* "$mnt_point/lib/modules/"
 
 # Required for mounting kernel modules at runtime
-# chroot -- "$mount_dir" systemctl enable netkit-mount.service
-# chroot -- "$mount_dir" systemctl enable netkit-unmount.service
+# chroot -- "$mnt_point" systemctl enable netkit-mount.service
+# chroot -- "$mnt_point" systemctl enable netkit-unmount.service
 
 
 ### USER MANAGEMENT ###########################################################
 # Set root to use no password
-chroot -- "$mount_dir" passwd --delete root
+chroot -- "$mnt_point" passwd --delete root
 
 # Update SSH keys in /etc/ssh to remove builder's hostname
-sed --in-place -- "s/$(whoami)@$(hostname)/root@netkit/g" "$mount_dir/etc/ssh/"*.pub
+sed --in-place -- "s/$(whoami)@$(hostname)/root@netkit/g" "$mnt_point/etc/ssh/"*.pub
 
 
 ### CLEANUP ###################################################################
 # Save debconf-package-selections
-chroot -- "$mount_dir" debconf-get-selections > "$work_dir/build/debconf-package-selections.last"
+chroot -- "$mnt_point" debconf-get-selections > "$work_dir/build/debconf-package-selections.last"
 
 # Empty package cache
-chroot -- "$mount_dir" apt-get clean
+chroot -- "$mnt_point" apt-get clean
 
 # Delete bootstrap log
-rm --force -- "$mount_dir/var/log/bootstrap.log"
+rm --force -- "$mnt_point/var/log/bootstrap.log"
