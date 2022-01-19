5adfe10775a3bb185595f1f10aadc8b6b11dffb5
Oshawk <oshawk@protonmail.com>
2022-01-19 13:38:23 -0800

    Add kitty tabs

---
+++ core/bin/kitty-tab.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Locking: http://mywiki.wooledge.org/BashFAQ/045
+
+if [ ! -S /tmp/nk_kitty ]
+then
+    if mkdir /tmp/nk_kitty_lock 2>/dev/null
+    then
+        trap 'rm -rf /tmp/nk_kitty_lock' 0
+
+        kitty \
+            --override allow_remote_control=yes \
+            --detach \
+            --listen-on unix:/tmp/nk_kitty \
+            /bin/bash -c "$1"
+        
+        while [ ! -S /tmp/nk_kitty ]
+        do
+            sleep 1
+        done
+
+        exit 0
+    else
+        while [ ! -S /tmp/nk_kitty ]
+        do
+            sleep 1
+        done
+    fi
+fi
+    
+kitty \
+    @ \
+    --to unix:/tmp/nk_kitty \
+    launch \
+    --type tab \
+    /bin/bash -c "$1" \
+    > /dev/null
